d634ce23ee0b9d9e043469387f07e6f2a71ca243
fajar sidik priatna <jardik.oryza@gmail.com>
2025-01-20 09:50:39 +0700

    update treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,39 +1,82 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "javascript", "typescript", "php", "go", "rust", },
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = { 
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "markdown",
+        "markdown_inline",
+        "javascript",
+        "typescript",
+        "php",
+        "go",
+        "rust",
+        "html",
+        "php_only",
+        "bash",
+        "css",
+        "vue",
+        "yaml",
+        "sql",
+        "rust",
+        "svelte",
+        "dockerfile",
+        "cpp",
+        "cmake",
+        "regex",
+    },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-  indent = {
-      enable = true
-  },
+    indent = {
+        enable = true
+    },
 
-  highlight = {
-    enable = true,
+    highlight = {
+        enable = true,
 
-    disable = function(lang, buf)
-        if lang == "html" then
-            print("disabled")
-            return true
-        end
+        disable = function(lang, buf)
+            if lang == "html" then
+                print("disabled")
+                return true
+            end
 
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            vim.notify(
-            "File larger than 100KB treesitter disabled for performance",
-            vim.log.levels.WARN,
-            {title = "Treesitter"}
-            )
-            return true
-        end
-    end,
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                vim.notify(
+                    "File larger than 100KB treesitter disabled for performance",
+                    vim.log.levels.WARN,
+                    { title = "Treesitter" }
+                )
+                return true
+            end
+        end,
 
-    additional_vim_regex_highlighting = false,
-  },
+        additional_vim_regex_highlighting = false,
+    },
 }
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+
+parser_config.blade = {
+    install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+    },
+    filetype = "blade"
+}
+
+vim.filetype.add({
+    pattern = {
+        ['.*%.blade%.php'] = 'blade',
+    }
+})
